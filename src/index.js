00ef9fcee9e66b10cd2d847dc706e961c39dc0a7
Peter Wang <happypeter1983@gmail.com>
2018-02-08 20:10:48 +0800

    rm store/ fix #3

--- src/index.js
+++ src/index.js
@@ -2,7 +2,17 @@ import React from 'react'
 import App from './containers/App'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
-import store from './store'
+import { createStore, applyMiddleware } from 'redux'
+import reducer from './reducers'
+import thunk from 'redux-thunk'
+import { createLogger } from 'redux-logger'
+
+const middleware = [thunk]
+if (process.env.NODE_ENV !== 'production') {
+  middleware.push(createLogger())
+}
+
+const store = createStore(reducer, applyMiddleware(...middleware))
 
 ReactDOM.render(
   <Provider store={store}>
--- src/store/index.js
+++
@@ -1,8 +0,0 @@
-import { createStore, applyMiddleware } from 'redux'
-import rootReducer from '../reducers'
-import logger from 'redux-logger'
-import thunk from 'redux-thunk'
-
-let middlewares = [thunk, logger]
-
-export default createStore(rootReducer, applyMiddleware(...middlewares))
