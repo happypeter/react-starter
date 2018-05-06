8f12b3a82794adcd201b10e1192ad40cc854d2de
Peter Wang <happypeter@users.noreply.github.com>
2018-05-06 17:59:24 +0800

    relative import later

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import App from './containers/App'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
-import reducer from './reducers'
 import thunk from 'redux-thunk'
+import reducer from './reducers'
+import App from './containers/App'
+
 
 let middleware = [thunk]
 if (process.env.NODE_ENV !== 'production') {
